85ec5b0c29d1b45dc161b5b711a18ad1a1221cfe
negibokken <negibokken@gmail.com>
2017-07-22 11:07:42 +0900

    WIP: Update

--- abstract_factory/src/index.ts
+++ abstract_factory/src/index.ts
@@ -1,7 +1,11 @@
 // AbstractFactory pattern
 // 話したいことメモ:
-// 部品を抽象化している
-// さらには手段までも抽象化しているんじゃないかという着想を得た
+// インスタンスの生成を専門に行うクラスを用意すると整合性が必要な**オブジェクト群を間違いなく生成**できる
+// メンテの都合や、拡張性を担保したいので部品classは抽象化して管理したい
+// そうすると、不都合なのが、Tireクラスがあったときに、BycycleTire, F1Tire どっちもはめられる
+// なので、車はこの部品を使うんだよということを規定して、もう一段階具体的にしておく。
+//
+// さらには手段までも抽象化しているんじゃないかという着想を得た (いや関係ないわw)
 // ぼくみたいなのがclass使うのはおこがましいと思ってたりしません？
 // なんか敷居が高いなぁと思ってたりしませんか？
 // class は使わないと書き方分からないし、どんどん使っていきましょう
@@ -10,18 +14,31 @@ class Pot {
 
 }
 
+class HotPot {
+  private pot: Pot;
+  private soup: Soup;
+  private protein: Protein;
+  constructor(pot: Pot) {
+    this.pot = pot;
+  }
+
+  // public void addSoup(Soup soup)
+  addSoup(soup: Soup): void {
+    this.soup = soup;
+  }
+
+  // public void addMain(Protein protein)
+  addMain(protein: Protein): void {
+    this.protein = protein;
+  }
+}
+
 class Soup {
 
 }
 
 class Protein {
 
-}
-
-class HotPot {
-  private Pot: Pot;
-  private Soup: Soup;
-  private Protein: Protein;
 }
 
 abstract class Factory {
