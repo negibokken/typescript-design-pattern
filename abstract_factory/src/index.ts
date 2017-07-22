e54c92803d370345a27fdd8ff27b6c41c1dac2dd
negibokken <negibokken@gmail.com>
2017-07-22 09:48:02 +0900

    WIP: Update file

--- abstract_factory/src/index.ts
+++ abstract_factory/src/index.ts
@@ -1,4 +1,28 @@
 // AbstractFactory pattern
+// 話したいことメモ:
+// 部品を抽象化している
+// さらには手段までも抽象化しているんじゃないかという着想を得た
+// ぼくみたいなのがclass使うのはおこがましいと思ってたりしません？
+// なんか敷居が高いなぁと思ってたりしませんか？
+// class は使わないと書き方分からないし、どんどん使っていきましょう
+
+class Pot {
+
+}
+
+class Soup {
+
+}
+
+class Protein {
+
+}
+
+class HotPot {
+  private Pot: Pot;
+  private Soup: Soup;
+  private Protein: Protein;
+}
 
 abstract class Factory {
   abstract getSoup(): string;
@@ -9,15 +33,15 @@ class MizutakiFactory extends Factory {
   getSoup(): string {
     return 'soup';
   }
-  // getSoup2(): string {
-  //   return 'soup2';
-  // }
+  getSoup2(): string {
+    return 'soup2';
+  }
 }
 
 // public static void main があるクラスに書いてね
 class Main {
   main(): void {
-    HotPot hotPot = newHotPot(new Pot())
+    let hotPot: HotPot = new HotPot(new Pot());
 
   }
 }
