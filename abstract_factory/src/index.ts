22091beed849228e225f2e86b8c813dc41aab54c
negibokken <negibokken@gmail.com>
2017-07-22 11:22:20 +0900

    Complete Abstract Factory

--- abstract_factory/src/index.ts
+++ abstract_factory/src/index.ts
@@ -2,17 +2,18 @@
 // 話したいことメモ:
 // インスタンスの生成を専門に行うクラスを用意すると整合性が必要な**オブジェクト群を間違いなく生成**できる
 // メンテの都合や、拡張性を担保したいので部品classは抽象化して管理したい
-// そうすると、不都合なのが、Tireクラスがあったときに、BycycleTire, F1Tire どっちもはめられる
-// なので、車はこの部品を使うんだよということを規定して、もう一段階具体的にしておく。
+// そうすると、不都合なのが、NormalCarクラスの中で Tireクラスを使うときに、BycycleTire, F1Tire どっちもはめられる
+// なので、車はこの部品はNormalCar工場から使うようにしておけばBycycleTireが出てきたりしなくなる
+// (改めて勉強してて思ったけど、NromalCarインスタンスを返す工場があってもいいのにね)
 //
-// さらには手段までも抽象化しているんじゃないかという着想を得た (いや関係ないわw)
+// さらには手段までも抽象化しているんじゃないかという着想を得た (いや関係なかった)
 // ぼくみたいなのがclass使うのはおこがましいと思ってたりしません？
 // なんか敷居が高いなぁと思ってたりしませんか？
 // class は使わないと書き方分からないし、どんどん使っていきましょう
 
-class Pot {
-
-}
+class Pot {}
+class Soup {}
+class Protein {}
 
 class HotPot {
   private pot: Pot;
@@ -21,44 +22,40 @@ class HotPot {
   constructor(pot: Pot) {
     this.pot = pot;
   }
-
   // public void addSoup(Soup soup)
   addSoup(soup: Soup): void {
+    // JS は特殊なので this を使うけど他の言語の人は違うので注意
     this.soup = soup;
   }
-
   // public void addMain(Protein protein)
   addMain(protein: Protein): void {
     this.protein = protein;
   }
 }
 
-class Soup {
-
-}
-
-class Protein {
-
-}
-
 abstract class Factory {
-  abstract getSoup(): string;
-  abstract getSoup2(): string;
+  abstract getSoup(): Soup;
+  abstract getMain(): Protein;
 }
 
 class MizutakiFactory extends Factory {
-  getSoup(): string {
-    return 'soup';
+  getSoup(): Soup {
+    return new Soup();
   }
-  getSoup2(): string {
-    return 'soup2';
+  getMain(): Protein {
+    return new Protein();
   }
 }
 
-// public static void main があるクラスに書いてね
+// 以下は public static void main がある部分に書いてね
 class Main {
   main(): void {
+    let factory: MizutakiFactory = new MizutakiFactory();
     let hotPot: HotPot = new HotPot(new Pot());
-
+    hotPot.addSoup(factory.getSoup());
+    hotPot.addMain(factory.getMain());
   }
 }
+
+let main: Main = new Main();
+main.main();
